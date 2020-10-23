c91a6b0fc3f2b387d6d0761486ed6a80a2106e32
Hertzler <DE1068@kasolvenzia.net>
2020-10-23 09:47:56 +0200

    mode change fixed Template profiles updated.

--- src/modules/LogitechInterfaceModule.lua
+++ src/modules/LogitechInterfaceModule.lua
@@ -21,6 +21,7 @@ end
 ---@param targ number | string | table
 ---@param fam string
 function LogitechInterfaceModule:_modeSelect(targ, fam)
+  local deviceState = tl.activeProfile.deviceState
   if type(fam) == "string" and fam == "all" then
     local famArr = {"m", "a", "l", "k"}
     for g = 1, #famArr do self:_modeSelect(targ, famArr[g]) end
@@ -29,22 +30,22 @@ function LogitechInterfaceModule:_modeSelect(targ, fam)
   else
     fam = tl.str:token(fam)
     if type(targ) == "table" then targ = targ[1] end
-    targ = tl.tbl:cycleIndex(tl.activeProfile.deviceState[fam].modeCount, targ, tl.deviceState[fam].modus)
-    if type(targ) ~= "number" or tl.deviceState[fam].modeCount < 2 or tl.deviceState[fam].modus == targ then return end
-    if tl.deviceState[fam].shift == 0 then self:syncModes(targ, nil, fam) end
+    targ = tl.tbl:cycleIndex(deviceState[fam].modeCount, targ, deviceState[fam].modus)
+    if type(targ) ~= "number" or deviceState[fam].modeCount < 2 or deviceState[fam].modus == targ then return end
+    if deviceState[fam].shift == 0 then self:syncModes(targ, nil, fam) end
     if targ == nil or targ == 0 then --if the target mode is 0, just cycle to the next mode
       _cycleMode(fam)
-    elseif targ <= tl.deviceState[fam].modeCount then --else cycle until you reach the target mode
-      while targ ~= tl.deviceState[fam].modus do _cycleMode(fam) end
+    elseif targ <= deviceState[fam].modeCount then --else cycle until you reach the target mode
+      while targ ~= deviceState[fam].modus do _cycleMode(fam) end
     else self:_modeSelect(tl.deviceState[fam].modeCount, fam) end
-    if not tl.config.keepNameOnLCD then
-      tl:put("changed to mode '" ..(tl.deviceState[fam].modeConfig[tl.deviceState[fam].modus][1] or tl.deviceState[fam].modus) .. "' for " .. self.unToken[fam])
+    if not tl.activeProfile.config.keepNameOnLCD then
+      tl:put("changed to mode '" ..(deviceState[fam].modeConfig[deviceState[fam].modus][1] or deviceState[fam].modus) .. "' for " .. self.unToken[fam])
     else
-      self:putNoLCD("changed to mode '" ..(tl.deviceState[fam].modeConfig[tl.deviceState[fam].modus][1] or tl.deviceState[fam].modus) .. "' for " .. self.unToken[fam])
+      self:putNoLCD("changed to mode '" ..(deviceState[fam].modeConfig[deviceState[fam].modus][1] or deviceState[fam].modus) .. "' for " .. self.unToken[fam])
       tl:put("")
     end
-    if tl.deviceState[fam].modeConfig[targ] and tl.deviceState[fam].modeConfig[targ][2] then
-      self:backLightControl(tl.deviceState[fam].modeConfig[targ][2], fam)
+    if deviceState[fam].modeConfig[targ] and deviceState[fam].modeConfig[targ][2] then
+      self:backLightControl(deviceState[fam].modeConfig[targ][2], fam)
     end
   end
 end
