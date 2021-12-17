bfd5b6e87378c60aaf89e2238ab15649a596a5a2
Timo Hertzler <timo.hertzler@gmail.com>
2021-12-17 21:10:59 +0100

    wha

--- src/modules/StringUtilitiesModule.lua
+++ src/modules/StringUtilitiesModule.lua
@@ -1,5 +1,5 @@
 local tl = ...---@type MainLibObject
-local lower, match, sub, type, concat, pairs, find, ceil, tonumber, error = tl.utf8.lower, tl.utf8.match, tl.utf8.sub, type, table.concat, pairs, tl.utf8.find, math.ceil, tonumber, error
+local lower, match, sub, type, concat, find, ceil, tonumber, error, pairs = tl.utf8.lower, tl.utf8.match, tl.utf8.sub, type, table.concat, tl.utf8.find, math.ceil, tonumber, error, pairs
 
 --=============================================================
 local StringUtilitiesModule = tl.baseClass:new()---@class StringUtilitiesModule:BaseClass Functions that process or type strings 
