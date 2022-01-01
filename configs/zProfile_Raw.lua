5bfc8e23acd86bfb8b003688a50798552fcdfb45
Timo Hertzler <timo.hertzler@gmail.com>
2022-01-01 21:28:14 +0100

    tl to rv 1

--- configs/zProfile_Raw.lua
+++ configs/zProfile_Raw.lua
@@ -1,18 +1,18 @@
-i, e = pcall((function() local tl = {}--->>> Script Configuration ================================================================================
-    tl.profileName = "Template" ---[*] Define your internal profile name here.
-    tl.path = "D:/t-lib_g600" ---[*] Path to plugin folder
-    tl.extPaths = { "profiles/ext_lua", "profiles/ext_work" } ---[*] What are the names of the folders
-    tl.childPaths = true ---[*] Are the folders for profile groups child folders of the main script folder?
-    tl.fileLocation = 1 ---[*] Does the current profile use an external file in any of the external paths?
-    tl.fileName = nil --- Load external file from separate work directory
-    tl.defaultDocPath = { path = "", prefix = "", suffix = "_doc", name = "" }
-    tl.defaultConfigPath = { path = "", prefix = "", suffix = "_config", name = "" }  --- does the documentation file have an altogether other name and/or path? (overwrites docSuffix option)
-    tl.configPath = "D:/t-lib_g600/configs"
+i, e = pcall((function() local rv = {}--->>> Script Configuration ================================================================================
+    rv.profileName = "Template" ---[*] Define your internal profile name here.
+    rv.path = "D:/t-lib_g600" ---[*] Path to plugin folder
+    rv.extPaths = { "profiles/ext_lua", "profiles/ext_work" } ---[*] What are the names of the folders
+    rv.childPaths = true ---[*] Are the folders for profile groups child folders of the main script folder?
+    rv.fileLocation = 1 ---[*] Does the current profile use an external file in any of the external paths?
+    rv.fileName = nil --- Load external file from separate work directory
+    rv.defaultDocPath = { path = "", prefix = "", suffix = "_doc", name = "" }
+    rv.defaultConfigPath = { path = "", prefix = "", suffix = "_config", name = "" }  --- does the documentation file have an altogether other name and/or path? (overwrites docSuffix option)
+    rv.configPath = "D:/t-lib_g600/configs"
     --->Path Config End =============================================================================================
-    function tl.profile(a) --->>> Define Internal Key Assignments and profile configs Here (external files are recommended)  ==============================================================================
+    function rv.profile(a) --->>> Define Internal Key Assignments and profile configs Here (external files are recommended)  ==============================================================================
 
         --->>> < of Assignment Program! =========================================================================
-    end loadfile(tl.path .. "/revenant.lua")():new(tl) end)) if (e) then OutputLogMessage("Error loading Revenant.\n" .. e .. ".\n") end --Main Program, do not touch
+    end loadfile(rv.path .. "/revenant.lua")():new(rv) end)) if (e) then OutputLogMessage("Error loading Revenant.\n" .. e .. ".\n") end --Main Program, do not touch
 --[[ Key Test 
     
         --]]
