f961682fbb818e86dcc6d3c9878ff54a65deb5cc
Timo Hertzler <timo.hertzler@eos.ksi.de>
2022-01-14 10:59:44 +0100

    python almost finsihed

--- start/macroExtractor/macroExtract.py
+++ start/macroExtractor/macroExtract.py
@@ -1,28 +1,45 @@
 import xml.etree.ElementTree as tree
 import os
+import re
 
 minDelay = 300
-xns = {'x':'*'}
 translate = True
+xns = {'x':'*'}
 
 keymapUS = {"0" : "0", "1" : "1", "2" : "2", "3" : "3", "4" : "4", "5" : "5", "6" : "6", "7" : "7", "8" : "8", "9" : "9", "a" : "a", "appkey" : "/m", "b" : "b", "backslash" : "\\", "backspace" : "/b", "c" : "c", "capslock" : "/L", "comma" : ",", "d" : "d", "down" : "/d", "e" : "e", "end" : "/E", "enter" : "\r", "equal" : "=", "escape" : "/e", "f" : "f", "f1" : "/01", "f10" : "/10", "f11" : "/11", "f12" : "/12", "f13" : "/13", "f14" : "/14", "f15" : "/15", "f16" : "/16", "f17" : "/17", "f18" : "/18", "f19" : "/19", "f2" : "/02", "f20" : "/20", "f21" : "/21", "f22" : "/22", "f23" : "/23", "f24" : "/24", "f3" : "/03", "f4" : "/04", "f5" : "/05", "f6" : "/06", "f7" : "/07", "f8" : "/08", "f9" : "/09", "g" : "g", "h" : "h", "home" : "/h", "i" : "i", "insert" : "/i", "j" : "j", "k" : "k", "l" : "l", "lalt" : "/a", "lbracket" : "[", "lctrl" : "/c", "left" : "/l", "lgui" : "/w", "lshift" : "/s", "m" : "m", "minus" : "-", "n" : "n", "numlock" : "/N", "o" : "o", "p" : "p", "pagedown" : "/D", "pageup" : "/U", "pause" : "/P", "period" : ".", "printscreen" : "/p", "q" : "q", "quote" : "'", "r" : "r", "ralt" : "/A", "rbracket" : "]", "rctrl" : "/C", "rgui" : "/W", "right" : "/r", "rshift" : "/S", "s" : "s", "semicolon" : ";", "slash" : "//", "spacebar" : " ", "t" : "t", "tab" : "\t", "tilde" : "`", "u" : "u", "up" : "/u", "v" : "v", "w" : "w", "x" : "x", "y" : "y", "z" : "z" }
 
+mods = {'/s':'~','/c':'*','/a':'#'}
+
 keymapQWERTZ = {"\\" : "/#", "[":"ü", "-":"ß","non_us_slash":"<","'":"ä","]":"+",";":"ö","//":"-","y":"z","z":"y"}
+
+capitalizer = lambda a : re.sub(r'(?<!(?:#|\*))~([a-z])',lambda match:match.group(1).upper(),a)
 
 def stringify(key):
    shortKey = keymapUS.get(key) or key
    if translate: shortKey = keymapQWERTZ.get(shortKey) or shortKey
    return shortKey
 
-def nextUp(index,arr):
+def nextUp(index,arr,manKey=0):
    if index + 1 == len(arr):return False 
-   downKey = arr[index]['key']
+   downKey = manKey or arr[index]['key']
    def upVersion(idx):
       return arr[idx]['dir'] == 'up' and arr[idx]['key'] == downKey
 
    if type(arr[index+1]) is not int and upVersion(index+1): return 1
    if type(arr[index+1]) is int and arr[index+1] < minDelay and upVersion(index+2):return 2
    return False
+
+def wrapping(index,arr):
+   if index + 1 == len(arr):return False 
+   downKey=arr[index]['key']
+   nextInt = type(arr[index+1]) is int
+   if nextInt and arr[index+1] > minDelay: return False
+   buffer = 1 if nextInt else 0
+   upVal = nextUp(index+buffer+1,arr)
+   if not upVal:return False
+   selfUp = nextUp(index+buffer+upVal+1,arr,downKey)
+   if not selfUp:return False
+   return [buffer+upVal+selfUp+1,arr[index+1+buffer]['key']]
 
 def macroExtract(path):
    root = tree.parse(path).getroot()
@@ -44,24 +61,29 @@ def macroExtract(path):
                entry= naiveList[i]
                if type(entry) is int and entry > minDelay:
                   if stringBuffer != '':
-                     condensed.append('"'+stringBuffer+'"')
+                     condensed.append(capitalizer('"'+stringBuffer+'"'))
                      stringBuffer= ''
                   condensed.append(str(entry))
                elif type(entry) is int:
                   pass
                else:
                   k = entry['key']
-                  upNum = nextUp(i,naiveList)
+                  upNum = nextUp(i,naiveList) if entry['dir'] == "down" else 0
+                  isMod = (not upNum) and mods.get(stringify(k))
+                  wraps = isMod and wrapping(i,naiveList)
                   if upNum:
                      i+=upNum
                      stringBuffer += stringify(k)
+                  elif wraps:
+                     i+=wraps[0]
+                     stringBuffer += isMod+stringify(wraps[1])
                   else:
                      if stringBuffer != '':
-                        condensed.append('"'+stringBuffer+'"')
+                        condensed.append(capitalizer('"'+stringBuffer+'"'))
                         stringBuffer= ''
                      condensed.append('{"'+stringify(k)+'", t="'+("u" if entry["dir"]=="up" else "d")+'"}')
                i+=1
-            if stringBuffer != '':condensed.append('"'+stringBuffer+'"')
+            if stringBuffer != '':condensed.append(capitalizer('"'+stringBuffer+'"'))
             macObj = {'content':', '.join(condensed), 'name':mac.get('name')}
 
             macObj['actionDelay'] = mac.get('repeatdelay')
