8f9cea920514597a881e2f667a11f7371eda6697
Timo Hertzler <timo.hertzler@gmail.com>
2022-01-16 22:40:01 +0100

    bye bye old keymacro execute

--- src/classes/macros/KeyMacro.lua
+++ src/classes/macros/KeyMacro.lua
@@ -1,5 +1,5 @@
 local rv = ...---@type Revenant
-local type, running, concat, rep = type, coroutine.running, table.concat, string.rep
+local type, concat, rep = type, table.concat, string.rep
 --=============================================================
 ---@class _KeyOptions:MacroOptions
 ---@field scope '"key"'|'"family"'|'"global"'
@@ -65,54 +65,6 @@ function KeyMacro:export(depth)
     depth = depth or 0
     local indent = rep("  ", depth)
     return indent .. self.titleExport .. '"' .. (type(self.command) == "table" and rv.str:unbreak(concat(self.command, '+')) or rv.str:unbreak(self.command)) .. '"'
-end
-
---TODO:Clean up this mess lol
----@param event Event
-function KeyMacro:executeOld(event)
-    local dir, vir, keyName, fam, num, triggerMode, toggled = event.direction, event.virtualType, event.keyName, event.family, event.keyNum, self.triggerMode, rv.profile.toggledMacroKeys
-    local press = self:keyPress(event)
-    press.forceSleep = true
-    local state = rv.profile.deviceState
-    local keyString = self.command
-    local releaseToggle = false
-    local runner = running()
-    if (runner and triggerMode == 0) or (vir and triggerMode == 0 and (vir == 1 or dir == nil)) then
-        if type(keyString) == "string" and (state[fam]["_b" .. num] or
-        not (rv.keys.keyboardDefinition[keyString] or rv.keyStates.logiKeys[keyString])) then rv.keys:typingDelegator(keyString, press)
-        else
-            if type(keyString) ~= "table" then keyString = { keyString } end
-            rv.keys:pressAndReleaseSequence(keyString, press)
-            releaseToggle = true
-        end
-    else
-        if (dir == "down" and triggerMode == 0) or triggerMode == 1 or
-        (triggerMode == 4 and (dir == "down" or vir)) or (triggerMode == 3 and toggled["_" .. keyName] == nil) then
-            if triggerMode == 3 then toggled["_" .. keyName] = 1
-            elseif triggerMode == 4 then
-                local wrapperTargets = { key = state[fam]["_b" .. num], family = state[fam], global = rv.profile.globalState }
-                local releaseWrapper = wrapperTargets[(self.options.scope) or "key"]
-                if not releaseWrapper then
-                    state[fam]["_b" .. num] = {}
-                    releaseWrapper = state[fam]["_b" .. num]
-                end
-                if not releaseWrapper.wrapperContent then releaseWrapper.wrapperContent = {} end
-                releaseWrapper.wrapperContent[#releaseWrapper.wrapperContent + 1] = keyString
-            end
-            if type(keyString) == "string" then rv.keys:press(rv.keys:applyStringBuffer(keyString, press), press)
-            elseif type(keyString) == "table" then rv.keys:pressSequence(keyString, press) end
-        elseif
-        (dir == "up" and triggerMode == 0) or triggerMode == 2 or (dir == "down" and triggerMode == 3 and toggled["_" .. keyName] ~= nil)
-        then
-            if triggerMode ~= 4 then releaseToggle = true end
-            if type(keyString) == "string" then rv.keys:release(rv.keys:applyStringBuffer(keyString, press), press)
-            elseif type(keyString) == "table" then
-                rv.keys:releaseSequence(keyString, press, self.options.unreverse)
-            end
-            if triggerMode == 3 then toggled["_" .. keyName] = nil end
-        end
-    end
-    if releaseToggle then rv.keys:autoRelease(press) end
 end
 
 ---@param event Event
