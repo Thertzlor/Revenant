e62732b9b0df2a156edbf6598bda7f2ed7cbddf7
Thertzlor <timo.hertzler@gmail.com>
2022-11-25 11:31:22 +0100

    cacle macro documented

--- src/classes/macros/CycleMacro.lua
+++ src/classes/macros/CycleMacro.lua
@@ -4,7 +4,7 @@ local type, GetRunningTime, abs, huge, rep, concat = type, GetRunningTime, math.
 ---@class _CycleOptions:MacroOptions
 ---@field inherit "all"| "none"| "timing"| "status" choose which attributes child cycles will inherit from their parents
 ---@field limit integer How many times the macro will play normally before finishing
----@field range {[1]:integer,[2]?:integer, [3]?:integer}
+---@field range {[1]:integer,[2]?:integer, [3]?:integer} start, initialize and end the cycle at specific positions
 ---@field interval integer how many steps the macro should advance after playing
 ---@field finish table|"stall"|"end"|"reset" what happens when the macro finishes
 ---@field cancel integer defines if and how a cycle can be cancelled.
@@ -41,22 +41,23 @@ CycleMacro.terminus = false
 ---@protected
 function CycleMacro:parseInstructions()
     self.keyData = {}
-    if self.options.limit == 0 or not self.options.limit then self.options.limit = huge end
+    if self.options.limit == 0 or not self.options.limit then self.options.limit = huge end --by default we cycle forever.
     self.options.inherit = self.options.inherit or "all"
     self.options.cancel = self.options.cancel or 0
-    self.options.finish = self.options.finish or "stall"
+    self.options.finish = self.options.finish or "stall" --upon finishing the cycle simply does nothing upon activation.
     self.unstable = (self.options.cancel == 1 or self.options.cancel < 0)
     self.command = {}
     local processed = 0
     local offset = 0
     local command = {}
     self.state.cyclesComplete = 0
+    ---setting the final table values after identifying all sub macros
     local function finalIteration()
         if self.init then return end
         self.command = command
         for i = 1, #self.command do local finCm = self.command[i]
             if finCm._ref then local ref = finCm._ref
-                self.command[i] = { ref }
+                self.command[i] = { ref } --we find the name of the sub macros and fetch their ids.
                 self:async(self.replaceWithReferenceId, self, ref, i, self.command, true)
             end
         end
@@ -115,7 +116,7 @@ function CycleMacro:execute(event)
     local options = self.options
     local meta = self.state
     if type(cycles) ~= "table" then return end
-    local step = 1
+    local step = 1 --how many positions were iterated in this execution
     local lim = options.limit
     local inherit = options.inherit
     local rupture = options.cancel
@@ -126,7 +127,7 @@ function CycleMacro:execute(event)
     local init = start
     local finish = #cycles
     if type(options.range) == "table" and rv.tbl:isSingleTypeTable(options.range, "number") then
-        local range = options.range
+        local range = options.range --modifying our start and finish variables according to the `range` option.
         for j = 1, range do if range[j] <= 0 then range[j] = #cycles + range[j] end end
         if range[2] and range[2] < #cycles then init = range[2] --[[@as integer]] end
         if range[1] < #cycles then start = range[1] end
@@ -134,45 +135,45 @@ function CycleMacro:execute(event)
         if finish > #cycles then finish = #cycles end
     end
     local directed = vir and 2 or 3
-    ---@type Event
-    local virtualEvent = self:virtualize(event, directed)
+    local virtualEvent = self:virtualize(event, directed) --sub macros receive a virtualized version of the original event.
     local press = self:keyPress(event) ---@type KeyPress
-    if meta.position == nil or (vir and dir == "down" and (rv.profile.macroIndex[parent].state.position == 1) and meta.cyclesComplete == 1 and inherit ~= "timing" and inherit ~= "none") then
+    if meta.position == nil or (vir and dir == "down" and (rv.profile.macroIndex[parent].state.position == 1) and meta.cyclesComplete == 1 and inherit ~= "timing" and inherit ~= "none") then --first execution of the macro
         meta.position = init
         meta.cyclesComplete = 1
         meta.cycleTimer = GetRunningTime()
     elseif rupture ~= 0 and rupture ~= 1 and (dir == "down") and (GetRunningTime() - meta.cycleTimer > abs(rupture)) then
+        --here, our cycle has been cancelleed, either by a timeout or by the press of another button.
         meta.position = init
         meta.cyclesComplete = 1
     end
-    if type(meta.cyclesComplete) == "number" and meta.cyclesComplete > lim then
-        if quitter == "end" then return
-        elseif quitter == "reset" then
+    if type(meta.cyclesComplete) == "number" and meta.cyclesComplete > lim then --reaching the end of the cycle.
+        if quitter == "end" then return --the `end` option simply aborts execution
+        elseif quitter == "reset" then --resetting a cycle after ending. This resets to the `init` position, not to `start`.
             meta.position = init
             meta.cyclesComplete = 1
-        elseif type(quitter) == "table" then
+        elseif type(quitter) == "table" then --the ending definition may be a reference to another macro to run
             rv.profile.macroIndex[quitter[1]]:run(virtualEvent)
             return
         end
     end
     if vir and virtParent and inherit ~= "status" and inherit ~= "none" then
-        meta.cycleTimer = (rv.profile.macroIndex[parent].state and rv.profile.macroIndex[parent].state.cycleTimer) or GetRunningTime()
-    else meta.cycleTimer = GetRunningTime() end
+        meta.cycleTimer = (rv.profile.macroIndex[parent].state and rv.profile.macroIndex[parent].state.cycleTimer) or GetRunningTime() --inheriting the cycle timer from the parent macro if applicable.
+    else meta.cycleTimer = GetRunningTime() end --saving our own cycle timer
     if meta.position ~= 1 or type(cycles[meta.position]) ~= "number" then
         local mac = cycles[meta.position]
-        local macType = type(mac)
+        local macType = type(mac) --any command is either a string to type or a macro to execute.
         if macType == "table" then rv.profile.macroIndex[mac[1]]:run(virtualEvent)
         elseif macType == "string" and (meta.matchUp or meta.matchDown) then rv.keys:typingDelegator(self.keyData[meta.position], press, (self.pID .. '_' .. meta.position)) end
     end
-    if dir == "up" or (vir and vir ~= 2 and vir ~= 3) then
+    if dir == "up" or (vir and vir ~= 2 and vir ~= 3) then --here we calculation the real `step` based on `interval`
         while type(cycles[meta.position + ((step + (interval)) - 1)]) == "number" do step = step + interval end
         meta.position = meta.position + ((step + interval) - 1)
-        if meta.position > finish or meta.position > #cycles then
+        if meta.position > finish or meta.position > #cycles then --nothing advances if we are already finished.
             if not (init > finish and meta.position <= #cycles and meta.cyclesComplete == 1) then
-                if meta.cyclesComplete < lim then
+                if meta.cyclesComplete < lim then --resetting loop back to start
                     meta.position = start + meta.position - finish - 1
                     meta.cyclesComplete = meta.cyclesComplete + 1
-                else
+                else --keeping track of the number of cycles
                     meta.cyclesComplete = lim + 1
                     meta.position = #cycles
                 end
@@ -198,7 +199,9 @@ function CycleMacro:setCyclesCompleted(number)
     self.state.cyclesComplete = number
 end
 
----interface function for control macro
+---interface function for control macro.
+---Unlike continuos macros, cycles can be controlled by setting their position
+---and the number of completed cycles.
 ---@param options l<integer>
 ---@param output boolean|number
 ---@param duration number
@@ -206,11 +209,11 @@ end
 function CycleMacro:control(options, output, duration, controlId)
     local positionOption = options
     local completedOption
-    if type(options) == "table" then
+    if type(options) == "table" then --with a table, both position and completion can be set at once.
         positionOption = options[1]
         completedOption = options[2]
     end ---@cast positionOption integer
-    if positionOption == 0 then self.state.position = nil
+    if positionOption == 0 then self.state.position = nil --a value of 0 forces a complete re-initialization
     elseif positionOption then self:setCyclePosition(positionOption) end
     if completedOption then self:setCyclesCompleted(completedOption) end
     if output then rv.lcd:displayOnLCD(self.pID .. '_' .. controlId, 1, duration) end
@@ -221,11 +224,11 @@ function CycleMacro:export(depth)
     depth = depth or 0
     local indent = rep("  ", depth)
     local nextIndent = rep("  ", depth + 1)
-    local subTable = {}
+    local subTable = {} --fetching sub macro exports
     for i = 1, #self.command do local cmd = self.command[i]
         subTable[#subTable + 1] = type(cmd) == "string" and (nextIndent .. '"' .. cmd .. '"') or rv.profile.macroIndex[cmd[1]]:export(depth + 1)
     end
-    local content = #subTable == 0 and false or "\n" .. concat(subTable, ",\n")
+    local content = #subTable == 0 and false or "\n" .. concat(subTable, ",\n") --exporting grouped export.
     return indent .. (self.titleExport or '') .. 'Cycle: (' .. (content or "") .. "\n" .. indent .. ")"
 end
 
