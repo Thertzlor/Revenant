f9dbedaaa8bd87caaf4ebc093ca02e442c7d2fae
Thertzlor <timo.hertzler@gmail.com>
2024-09-18 14:54:13 +0200

    instance sync

--- src/macros/InstanceMacro.lua
+++ src/macros/InstanceMacro.lua
@@ -12,6 +12,7 @@ local remove, type, insert, next, abs, pairs, error = table.remove, type, table.
 ---@field update? UpdateDefinition #Definition object for a modification of the instance
 ---@field newType? MacroType #change the macro type of the created instance
 ---@field noDefaults? boolean #don't inherit default options of the profile/scope
+---@field substitute? table<number|string,any> # Every key in the target macro (including child macros) corresponding to a key of this table will be substituted with the key's value.
 --[[=============================================================]] --
 ---@class UpdateDefinition:{[1]:any}
 ---@field source? string #The name of the macro the update data is sourced from
@@ -21,6 +22,7 @@ local remove, type, insert, next, abs, pairs, error = table.remove, type, table.
 --[[=============================================================]] --
 ---@class __InstanceShorthands
 ---@field u? UpdateDefinition #shorthand for "update"
+---@field sub? table<number|string,any> #shorthand for "substitute"
 --[[=============================================================]] --
 ---Assign a macro that creates a new independent instance of another macro, optionally modifying its functionality.
 ---@alias AssignInstance MacroInitDefinition<"instance","i",_InstanceOptions|__InstanceShorthands>
@@ -35,11 +37,12 @@ InstanceMacro.type = "instance"
 InstanceMacro.lintProperties = { ---@type OptionsLintPreset
    update = {type = "table", tableKeys = {"number", "string"}},
    newType = {type = "string"},
+   substitute = {type = "table", tableKeys = {"number", "string"}},
    noDefaults = {type = "boolean"},
    __all = true
 }
 InstanceMacro.lintCommand = {type = "string"}
-InstanceMacro.shorthands = {u = "update"}
+InstanceMacro.shorthands = {u = "update", sub = "substitute"}
 InstanceMacro.terminus = false
 
 local numericMethods = rv.tbl:propsFrom{"insert", "listinsert", "listreplace"}
@@ -66,8 +69,9 @@ end
 ---Duplicating and updating a new instance of a macro
 ---@param update UpdateDefinition[]
 ---@param target MacroInitDefinition
+---@param substitutions? table<number|string,any>
 ---@async
-function InstanceMacro:updateMain(update, target)
+function InstanceMacro:updateMain(update, substitutions, target)
    local total = #update
    local processed = 0
 
@@ -129,16 +133,18 @@ function InstanceMacro:updateMain(update, target)
          processContent(method, selector, subject)
       end
       processed = processed + 1
-      if processed == total then self:finalize(target) end
+      if processed == total then self:finalize(target, substitutions) end
    end
    for i = 1, total do self:async(advancedUpdate, update[i]) end
 end
 
 ---@private
 ---@param newRaw MacroInitDefinition|{n?:string}
+---@param subs? table<number|string,any>
 ---@async
-function InstanceMacro:finalize(newRaw)
+function InstanceMacro:finalize(newRaw, subs)
    if self.init then return end
+   if subs then self:substitute(newRaw, subs) end
    local subClass = rv.tbl:getMacroClass(newRaw) -- the new macro could be of another type than before
    if not subClass then error("Could not construct Macro for instance") end
    local defaultOptions = self.options
@@ -152,6 +158,20 @@ function InstanceMacro:finalize(newRaw)
    self:finishInit(true)
 end
 
+---@private
+---@param tab table<any,any>
+---@param subtab? table<string|number,any>
+function InstanceMacro:substitute(tab, subtab)
+   local sub = subtab or self.options.substitute or {}
+   for k, v in next, tab do
+      if sub[v] ~= nil then
+         tab[k] = sub[v]
+      elseif type(v) == "table" then
+         self:substitute(tab[k], sub)
+      end
+   end
+end
+
 ---@protected
 ---@async
 function InstanceMacro:parseInstructions()
@@ -163,15 +183,18 @@ function InstanceMacro:parseInstructions()
    else
       local myUpdate = self.options.update
       local newType = self.options.newType
+      local substitutes = self.options.substitute
       self.options.newType = nil
       self.options.update = nil
+      self.options.substitute = nil
       local newRaw = rv.utils.deepCopy(rv.tbl:intersect({}, target.raw)) -- making sure we get a 'clean' table
+      newRaw.template = nil -- the result of an instance table is no longer a template.
       if newType then newRaw.type = newType end
       if myUpdate then
          local updates = (myUpdate.selector ~= nil or myUpdate.s ~= nil) and {myUpdate} or myUpdate
-         self:updateMain(updates, newRaw) -- updating the instance, option overrides don't require upating.
+         self:updateMain(updates, substitutes, newRaw) -- updating the instance, option overrides don't require upating.
       else
-         self:finalize(newRaw)
+         self:finalize(newRaw, substitutes)
       end
    end
 end
