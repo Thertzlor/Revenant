4892d04abbb5cf0ea1fc3d48bf53439f9ce84924
Thertzlor <timo.hertzler@gmail.com>
2024-09-18 14:55:45 +0200

    catching more template errors

--- src/classes/MacroDefinition.lua
+++ src/classes/MacroDefinition.lua
@@ -131,10 +131,10 @@ local toMain = {{"type", "key"}, "name", {"direction", "normal"}} ---Default val
 ---@field blocked boolean #True if a previous macro is currently blocking this macro's execution
 ---@field type string #The type of the macro
 ---@field name string #The display name of this macro
+---@field template boolean #True
 ---@field new fun(self:MacroDefinition, macroSummary?:MacroInitDefinition, defaults?:MacroInitDefinition, device?:HardwareDefinition, stack?:string[], scope?:string):MacroDefinition
 ---@field private lintProperties OptionsLintPreset #Type definition to veryify the integrity of the macro options
 ---@field private idThread thread #Thread on which the macro returns its own id
----@field private template boolean #True
 ---@field private lintCommand LintEntry #Type definition to verify the integrity of the macro command
 ---@field private dibs boolean #this is the first macro called for a specific name.
 ---@field private additiveDocs boolean #Documentation will export the default export in addition to the manual doc.
@@ -211,7 +211,11 @@ function MacroDefinition:constructor(macroSummary, defaults, device, stack, scop
       self.pID = self:genId()
       self:callDibs()
    end
-   if self.template then return self:finishInit() end
+   if self.template then
+      if not self.name then error("A template without a name can not be referenced or run, this is probably a mistake") end
+      self.titleExport = self.name or ""
+      return self:finishInit()
+   end
    self:parseQualifiers()
    self.msgDuration = (self.rawOptions.lcd and type(self.rawOptions.lcd) == "number") and self.rawOptions.lcd or rv.profile.config.LCDMessageDuration
    self.manualDocumentation = self.options.documentation or rv.profile.documentation[self.name]
@@ -520,7 +524,7 @@ function MacroDefinition:errorHandler(msg)
       name = "Macro " .. name
    end -- tracing the location of the current macro
    if not name then name = "a " .. self.type .. " macro" end
-   rv.states.scriptStates.errors[#rv.states.scriptStates.errors + 1] = name .. " failed to initialize:\n  " .. msg
+   rv.states.scriptStates.errors[#rv.states.scriptStates.errors + 1] = name .. " failed to initialize:\n  " .. (msg or "(No error message provided)")
 end
 
 ---@protected
