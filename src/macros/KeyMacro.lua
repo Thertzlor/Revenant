11559194d4f9d6e271d79ed4758d4eaa9cbd3a3c
Thertzlor <timo.hertzler@gmail.com>
2024-10-04 20:18:45 +0200

    wrapping tweaks

--- src/macros/KeyMacro.lua
+++ src/macros/KeyMacro.lua
@@ -2,18 +2,23 @@ local rv = ... ---@type Revenant
 local type, concat, assert, super = type, table.concat, assert, rv.importer:classImport("MacroDefinition")
 --[[=============================================================]] --
 ---@class _KeyOptions:MacroOptions
----@field scope? "key"|"family"|"global"  #Should the `wrapKey` macro affect all following key outputs or just ones from the same device or key?
 ---@field unreverse? boolean #Normally buttons are released in reverse order, set this to `true` to release them in the same order they were pressed.
 ---@field allKeys? boolean #all keys ever
 --[[=============================================================]] --
+---@class _WrapKeyOptions:MacroOptions
+---@field scope? "key"|"family"|"global"  #Should the `wrapKey` macro affect all following key outputs or just ones from the same device or key?
+--[[=============================================================]] --
 ---Assign a Macro that handles the default key functions, it can also be called by key name or as simple sequence.
----@alias AssignKey MacroInitDefinition<"key"|"keyup"|"keydown"|"wrapkey","k"|"u"|"d"|"w",_KeyOptions,(string|LogiKeyName)[]>
+---@alias AssignKey MacroInitDefinition<"key"|"keyup"|"keydown","k"|"u"|"d",_KeyOptions,(string|LogiKeyName)[]>
+--[[=============================================================]] --
+---Assign a Macro that defines one or more key inputs that will be pressed and wrapped around the next key output.
+---@alias AssignWrapKey MacroInitDefinition<"wrapkey","w",_WrapKeyOptions,(string|LogiKeyName)[]>
 --[[=============================================================]] --
 ---@class KeyMacro:MacroDefinition #Handles the default key functions, called by key name or as simple sequence.
 ---@field command l<string>
 ---@field keys KeyObject|KeyObject[]
 ---@field firstModifiers string[]|false
----@field options _KeyOptions
+---@field options _KeyOptions|_WrapKeyOptions
 ---@field naturalKey boolean
 ---@field triggerMode 0|1|2|3|4
 local KeyMacro = super:new()
@@ -34,7 +39,8 @@ KeyMacro.lintCommand = {type = "string"}
 function KeyMacro:parseInstructions()
    local triggerModes = {keydown = 1, keyup = 2, keytoggle = 3, wrapkey = 4}
    self.triggerMode = triggerModes[self.type] or 0
-   self.singleTrigger = self.triggerMode ~= 0
+   local mode = self.triggerMode
+   self.singleTrigger = mode ~= 0
    local cmd = self.command
    assert(cmd and (self.options.allKeys or #cmd ~= 0), "Key macro cannot be empty!")
    if #cmd == 1 then cmd = cmd[1] --[[@as string]] end
@@ -43,7 +49,7 @@ function KeyMacro:parseInstructions()
       self.keys = ({} --[[@as KeyObject[] ]] )
       for _, p in pairs(rv.keys.keyboardDefinition) do if p.key and not p.modifier then self.keys[#self.keys + 1] = p end end
    elseif type(cmd) == "string" then
-      self.keys = rv.keys:parseKeyName(cmd) or rv.keys:keyParser(cmd)
+      self.keys = (mode ~= 4 and rv.keys:parseKeyName(cmd)) or rv.keys:keyParser(cmd, mode == 4)
    else
       local keyCollection = {} ---@type KeyObject[]
       self.naturalKey = true
@@ -88,7 +94,7 @@ function KeyMacro:execute(event)
                rv.keys:press(keys, press)
             end
          else -- for when the string is not a key name
-            rv.keys:typingDelegator(keys, press, self.pID, true)
+            rv.keys:typingDelegator(keys, press, self.pID, true, noReverse)
             rv.keys:unwrap(press, noReverse)
             self:unBuffer()
          end
