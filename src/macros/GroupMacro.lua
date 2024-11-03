ae5d3d0bc0129a6225af59cbe766249e9063c21d
Thertzlor <timo.hertzler@gmail.com>
2024-11-03 17:42:13 +0100

    slight adjustment

--- src/macros/GroupMacro.lua
+++ src/macros/GroupMacro.lua
@@ -84,7 +84,7 @@ end
 ---@async
 function GroupMacro:run(event)
    if self.disabled then return end
-   ---If we have manually defined documentation, we won't let docMode iterate over sub macros, we just output rigth away.
+   ---If we have manually defined documentation, we won't let docMode iterate over sub macros, we just output right away.
    if rv.states.scriptStates.docMode and self.manualDocumentation then return rv.lcd:displayOnLCD(self.pID, 1) end
    local linked = event.link
    event.link = nil
