489e3199745cf4baf8575ba4b2253a1593db92b2
Thertzlor <timo.hertzler@gmail.com>
2026-09-12 20:17:24 +0200

    formatting, reference template update.

--- start/reference_LGS_template.lua
+++ start/reference_LGS_template.lua
@@ -1,10 +1,10 @@
-local _,e=pcall((function()local rv={}--->>> Script Configuration --================================================================================   
+local _,e=pcall((function()local rv={}--->>> Script Configuration --================================================================================
 
---- Any field prefixed with [*] NEEDS to be filled for Revenant to work.
-rv.profileName = "Template" ---[*] Define your internal profile name here.
-rv.path = "C:/Revenant" ---[*] Path to the folder in which Revenant is installed
-rv.profilePath = "@rv/profiles" ---[*] name of the profile folder
-rv.externalProfile = false ---[*] Does the current profile use an external file?
+                                 ---Any field prefixed with [*] NEEDS to be filled for Revenant to work.
+rv.profileName = "Template"      ---[*] Define your internal profile name here.
+rv.path = "./revenant"           ---[*] Path to the folder in which Revenant is installed, can be relative to LGS install path.
+rv.profilePath = "@rv/profiles"  ---[*] path of the profile directory, "@rv" means relative to the Revenant root directory.
+rv.externalProfile = false       ---[*] Does the current profile use an external file?
 rv.defaultDocPath = {prefix = "", suffix = "_doc"}
 rv.defaultConfigPath = {prefix = "", suffix = "_config"} ---does the documentation file have an altogether other name and/or path? (overwrites docSuffix option)
 rv.configPath = nil
